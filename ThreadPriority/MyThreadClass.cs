78978e651ce3ca6468a716510192ec0665ef3121
mulano123 <joshuamulano26@gmail.com>
2023-10-27 17:53:29 +0800

    final

--- ThreadPriority/MyThreadClass.cs
+++ ThreadPriority/MyThreadClass.cs
@@ -22,13 +22,14 @@ namespace ThreadPriority
 
         public static void Thread2()
         {
-            for (int j = 0; j < 6; j++)
+            for (int j = 0; j <= 5; j++)
             {
                 Thread thread = Thread.CurrentThread;
                 Console.WriteLine("Name of Thread: " + thread.Name + " = " + j);
                 Thread.Sleep(1500);
             }
         }
+
 
     }
 }
